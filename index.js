6c8f5c17b37543fecc2c96b9c84fe4d8f694d8e9
Adriana Sandoval <adrianaaasr@gmail.com>
2022-04-26 12:48:07 -0500

    changes

---
+++ index.js
@@ -0,0 +1,21 @@
+
+    function funcion1() {
+        alert("The best color in the whole world is the one that looks good on you. " );
+    }
+    
+    function funcion2() {
+        alert("If I ever let my head down, it will be just to admire my shoes. " );
+    }
+
+    function funcion3() {
+        alert("Decide what you are, what you want to express by the way you dress and the way you live. " );
+    }
+
+    function funcion4() {
+        alert("It's a new era in fashion- there are no rules. " );
+    }
+
+    function funcion5() { 
+        var name = prompt ("¿Escribe como te llamas?", "");
+        alert ("Hola " + name );
+    }
--- js/index.js
+++
@@ -1,9 +0,0 @@
-let icon = new Icon();
-icon.initializeGallery();
-
-const artPieces = icon.getPieces();
-
-function showInfoByClickButton(){
-  alert( artPieces[0].getDescription() )
-};
-
